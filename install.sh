926e41073d50afe7cac5430f6713fd925644c5b5
Robert Kotecki <seemerlin@o2.pl>
2016-11-24 17:13:26 +0100

    Update docker version

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -e docker-compose ]; then
-	curl -L https://github.com/docker/compose/releases/download/1.3.2/docker-compose-`uname -s`-`uname -m` > docker-compose
+	curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > docker-compose
 fi
 chmod +x docker-compose
 ./docker-compose up -d
